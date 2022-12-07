e8e4d952ad96fdc8bb965d62dc30a6cd49f4f132
SpawnSkull <saposuper_14@hotmail.com>
2022-12-07 11:57:07 -0600

    feat: Added service history

---
+++ app/src/main/java/mx/edu/utez/dealc/view/ServiceHistoryActivity.kt
@@ -0,0 +1,50 @@
+package mx.edu.utez.dealc.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import mx.edu.utez.dealc.databinding.ActivityServiceHistoryBinding
+import mx.edu.utez.dealc.adapter.ServiceProviderRequestAdapter
+import mx.edu.utez.dealc.model.ServiceProviderRequest
+
+class ServiceHistoryActivity : AppCompatActivity(), ServiceProviderRequestAdapter.Eventos {
+
+    lateinit var binding: ActivityServiceHistoryBinding
+    lateinit var adapter: ServiceProviderRequestAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityServiceHistoryBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setData()
+    }
+
+    private fun setData () {
+        binding.recyclerViewServiceHistory.layoutManager = LinearLayoutManager(this)
+        adapter = ServiceProviderRequestAdapter(this, this)
+        binding.recyclerViewServiceHistory.adapter = adapter
+
+        val list: List<ServiceProviderRequest> = listOf(
+            ServiceProviderRequest("", "Servicio 1", "1", "1", 2, "Pendiente", mutableListOf(), mutableListOf()),
+            ServiceProviderRequest("", "Servicio 2", "2", "2", 2, "Pendiente", mutableListOf(), mutableListOf()),
+            ServiceProviderRequest("", "Servicio 3", "3", "3", 2, "Pendiente", mutableListOf(), mutableListOf()),
+            ServiceProviderRequest("", "Servicio 4", "4", "4", 2, "Pendiente", mutableListOf(), mutableListOf()),
+        )
+
+        adapter.submitList(list)
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun onItemClick(element: ServiceProviderRequest, position: Int) {
+    }
+
+    override fun onChatClick(element: ServiceProviderRequest) {
+    }
+
+    override fun onAcceptClick(element: ServiceProviderRequest) {
+    }
+
+    override fun onRejectClick(element: ServiceProviderRequest) {
+    }
+}
